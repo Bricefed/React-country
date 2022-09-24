fee642b860c600e38a381ebecbc77375c32edcd7
Fedou Brice <napofedoupro@gmail.com>
2022-09-24 10:58:03 +0200

    Setup router and sass

---
+++ src/pages/About.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>À propos</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint placeat
+        aut odio. Quibusdam adipisci dolorem at dolore qui delectus ipsam
+        quisquam, expedita sunt tempora excepturi blanditiis aspernatur alias,
+        natus doloribus illo quae molestias. Dignissimos quod libero, provident
+        deleniti, nulla porro labore sequi unde, voluptas vitae placeat
+        voluptatum. Corrupti libero doloremque eveniet earum sequi aliquid id a
+        doloribus non, voluptate at praesentium, molestias eum pariatur neque
+        quas minus est ullam similique cum repellendus commodi ducimus illo
+        tempore. Vero, ut optio perferendis nobis deserunt inventore at
+        voluptatum aspernatur sint repellendus magnam. Expedita eligendi esse
+        possimus minima. Pariatur ducimus soluta consequatur modi explicabo.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aliquid
+        non animi vitae provident, libero nam temporibus magni officiis sequi
+        facere obcaecati pariatur nihil tempore mollitia odit et sunt dolore!
+        Fugiat, laboriosam eveniet. Repudiandae, quo ab repellendus, minus
+        cupiditate distinctio similique nesciunt nobis debitis rerum delectus
+        hic possimus nam odit?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = () => {
+  return <div>URL Inconue...</div>;
+};
+
+export default Error;
